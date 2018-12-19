d20c61f2c00f3bf9b158abda78037d4d150fe3c5
Gustavo <gustavocsalvador@gmail.com>
2018-12-19 14:45:10 -0200

    More stuff

--- cypress/integration/theme_publish.js
+++ cypress/integration/theme_publish.js
@@ -1,14 +1,20 @@
+const apikey = Cypress.env('APIKEY')
+const version = Cypress.env('VERSION')
+const username = Cypress.env('USERNAME')
+const password = Cypress.env('PASSWORD')
+
 describe('Rundeck', () => {
   it('login', () => {
     cy
       .visit('https://rundeck.chaordicsystems.com/user/login')
-      .get('#login').type('gustavo.salvador')
-      .get('#password').type('#ChAoRdIcGG498@')
+      .get('#login').type(username)
+      .get('#password').type(password)
       .get('.btn-primary').click()
       .visit('https://rundeck.chaordicsystems.com/project/prd-operations-tools/job/show/572bef4a-8b01-4533-b915-9dce7d518b86')
-      .get('[name~=\'extra.option.APIKEY\']').type('qdb-vtex')
-      .get('[name~=\'extra.option.VERSION\']').type('v1.0.139')
+      .get('[name~=\'extra.option.APIKEY\']').type(apikey)
+      .get('[name~=\'extra.option.VERSION\']').type(version)
       .get('#execFormRunButton').click()
       .get('#tab_link_output').click()
+      expect('#tab_link_output').to.exist
   })  
 })
