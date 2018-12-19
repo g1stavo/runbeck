f0e8d201fdc41ab06e7a618fef4ed44fcdef35b7
Gustavo <gustavocsalvador@gmail.com>
2018-12-19 15:08:18 -0200

    Small fixes

--- cypress/integration/theme_publish.js
+++ cypress/integration/theme_publish.js
@@ -1,18 +1,13 @@
-const apikey = Cypress.env('APIKEY')
-const version = Cypress.env('VERSION')
-const username = Cypress.env('USERNAME')
-const password = Cypress.env('PASSWORD')
-
-describe('Rundeck', () => {
-  it('login', () => {
+describe('theme_publish', () => {
+  it('job execution', () => {
     cy
       .visit('https://rundeck.chaordicsystems.com/user/login')
-      .get('#login').type(username)
-      .get('#password').type(password)
+      .get('#login').type(Cypress.env('USERNAME'))
+      .get('#password').type(Cypress.env('PASSWORD'))
       .get('.btn-primary').click()
       .visit('https://rundeck.chaordicsystems.com/project/prd-operations-tools/job/show/572bef4a-8b01-4533-b915-9dce7d518b86')
-      .get('[name~=\'extra.option.APIKEY\']').type(apikey)
-      .get('[name~=\'extra.option.VERSION\']').type(version)
+      .get('[name~=\'extra.option.APIKEY\']').type(Cypress.env('APIKEY'))
+      .get('[name~=\'extra.option.VERSION\']').type(Cypress.env('VERSION'))
       .get('#execFormRunButton').click()
       .get('#tab_link_output').click()
       expect('#tab_link_output').to.exist
